015e42ae8a707cbbf69e8e913cd3457e2498b515
JesusTouchMe <dodoakmishot@gmail.com>
2026-03-15 17:05:51 +0100

    feat: remove flag32 and flag64 types in favor of just using u32 and u64

--- include/libos/defines.h
+++ include/libos/defines.h
@@ -19,42 +19,39 @@ extern "C" {
 //      Once the libc dependency is gone, maybe use this header for all the declarations of libc-ish utilities.
 //      Just spitting ideas for now.
 
-typedef int8_t      os_i8;
-typedef int16_t     os_i16;
-typedef int32_t     os_i32;
-typedef int64_t     os_i64;
-typedef uint8_t     os_u8;
-typedef uint16_t    os_u16;
-typedef uint32_t    os_u32;
-typedef uint64_t    os_u64;
+typedef int8_t          os_i8;
+typedef int16_t         os_i16;
+typedef int32_t         os_i32;
+typedef int64_t         os_i64;
+typedef uint8_t         os_u8;
+typedef uint16_t        os_u16;
+typedef uint32_t        os_u32;
+typedef uint64_t        os_u64;
 
-typedef float       os_f32;
-typedef double      os_f64;
+typedef float           os_f32;
+typedef double          os_f64;
 
-typedef os_u8       os_bool;
+typedef os_u8           os_bool;
 
-typedef char        os_char;
-typedef os_char*    os_string; // All strings in the libos api are UTF-8 encoded and NUL terminated.
+typedef char            os_char;
+typedef os_char*        os_string; // All strings in the libos api are UTF-8 encoded and NUL terminated.
+typedef const os_char*  os_cstring; // c as in constant
 
 // These two help with compatability (cough cough Windows).
-typedef os_u16      os_char16;
-typedef os_u32      os_char32;
+typedef os_u16          os_char16;
+typedef os_u32          os_char32;
 
-typedef size_t      os_size;
-typedef ssize_t     os_ssize;
+typedef size_t          os_size;
+typedef ssize_t         os_ssize;
 
-typedef uintptr_t   os_uptr;
-typedef intptr_t    os_iptr;
+typedef uintptr_t       os_uptr;
+typedef intptr_t        os_iptr;
 
-// These won't be used raw, but rather for typedef-ing specific flag types in each subsystem.
-typedef os_u32      os_flags32;
-typedef os_u64      os_flags64;
+typedef os_i32          os_result;
 
-typedef os_i32      os_result;
+typedef void*           os_handle; // Can in theory hold any typed handle as long as your compiler is chill with implicit casts. You may want to enable the LIBOS_SUPERSAFE_HANDLES feature if you unironically prefer this.
 
-typedef void*       os_handle; // Can in theory hold any typed handle as long as your compiler is chill with implicit casts. You may want to enable the LIBOS_SUPERSAFE_HANDLES feature if you unironically prefer this.
-
-os_result __os_set_and_return_result(os_result result); // INTERNAL USE ONLY. Sets the last error in the current thread to `result` if `result` is less than 0, then returns the same result.
+LIBOS_EXPORT os_result __os_set_and_return_result(os_result result); // INTERNAL USE ONLY. Sets the last error in the current thread to `result` if `result` is less than 0, then returns the same result.
 
 #ifdef __cplusplus
 }
