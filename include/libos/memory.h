6d0c7c8fa66dcd1c566b043295ca320dac704167
JesusTouchMe <dodoakmishot@gmail.com>
2026-03-22 17:53:03 +0100

    feat(memory): add basic memory api with allocate and free
    Untested.

--- include/libos/memory.h
+++ include/libos/memory.h
@@ -1,9 +1,12 @@
 // Copyright 2026 Jannik Laugmand Bülow
 
+// I really like the Windows API approach to virtual memory, so you may notice some slight similarities.
+
 #ifndef LIBOS_MEMORY_H
 #define LIBOS_MEMORY_H 1
 
 #include "libos/defines.h"
+#include "libos/error.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,9 +15,28 @@ extern "C" {
 enum os_mem_alloc_intents {
     OS_MEM_RESERVE      = 1 << 0,
     OS_MEM_COMMIT       = 1 << 1,
-    OS_MEM_LARGE_PAGES  = 1 << 2,
+    OS_MEM_LARGE_PAGES  = 1 << 2, // If you specify this intent, you must also specify RESERVE and COMMIT
+
     OS_MEM_ZERO         = 1 << 3,
 };
+
+enum os_mem_free_intents {
+    OS_MEM_DECOMMIT     = 1 << 0,
+    OS_MEM_RELEASE      = 1 << 1,
+};
+
+enum os_mem_protect_intents {
+    OS_MEM_PROTECT_NOACCESS             = 1 << 0,
+    OS_MEM_PROTECT_READ                 = 1 << 1,
+    OS_MEM_PROTECT_READWRITE            = 1 << 2, //NOTE: I am a fan of having the control to make write-only memory, but please name one situation where that is needed.
+    OS_MEM_PROTECT_EXECUTE              = 1 << 3,
+    OS_MEM_PROTECT_EXECUTE_READ         = OS_MEM_PROTECT_EXECUTE | OS_MEM_PROTECT_READ,
+    OS_MEM_PROTECT_EXECUTE_READWRITE    = OS_MEM_PROTECT_EXECUTE | OS_MEM_PROTECT_READWRITE,
+};
+
+typedef os_u32 os_mem_alloc_intents;
+typedef os_u32 os_mem_free_intents;
+typedef os_u32 os_mem_protect_intents;
 
 LIBOS_EXPORT void os_mem_init(void);
 
@@ -22,6 +44,9 @@ LIBOS_EXPORT os_size os_mem_getpagesize(void);
 LIBOS_EXPORT os_size os_mem_getlargepagesize(void);
 LIBOS_EXPORT os_size os_mem_aligntopagesize(os_size x);
 LIBOS_EXPORT os_size os_mem_aligntolargepagesize(os_size x);
+
+LIBOS_EXPORT os_result os_mem_allocate(void** out_pointer, void* start_address, os_size size, os_mem_alloc_intents alloc_intents, os_mem_protect_intents protect_intents);
+LIBOS_EXPORT os_result os_mem_free(void* address, os_size size, os_mem_free_intents free_intents);
 
 #ifdef __cplusplus
 }
--- src/windows/memory.c
+++ src/windows/memory.c
@@ -9,6 +9,7 @@ static SYSTEM_INFO si;
 
 #ifndef LIBOS_OPT_ASSUME_POW2_PAGESIZE
 static os_size (*aligntopagesize)(os_size, os_size) = NULL;
+
 static os_size (*aligntolargepagesize)(os_size, os_size) = NULL;
 
 static os_size alignto_normal(os_size x, os_size a) {
@@ -61,4 +62,91 @@ os_size os_mem_aligntolargepagesize(os_size x) {
 #else
     return aligntopagesize(x, GetLargePageMinimum());
 #endif
+}
+
+// Converts intents into a 3-bit mask, then checks a table.
+// R W X
+static os_result translate_protect_intents(DWORD* out_flags, os_mem_protect_intents intents) {
+    static DWORD win_protect_table[8] = {
+        PAGE_NOACCESS, // ---
+        PAGE_READONLY, // R--
+        PAGE_READWRITE, // -W-   write implies read in this basic ahh table
+        PAGE_READWRITE, // RW-
+        PAGE_EXECUTE, // --X
+        PAGE_EXECUTE_READ, // R-X
+        PAGE_EXECUTE_READWRITE, // -WX
+        PAGE_EXECUTE_READWRITE, // RWX
+    };
+
+    os_u8 mask =
+            ((intents & OS_MEM_PROTECT_READ) ? 1 : 0) |
+            ((intents & OS_MEM_PROTECT_READWRITE) ? 2 : 0) |
+            ((intents & OS_MEM_PROTECT_EXECUTE) ? 4 : 0);
+
+    __LIBOS_OUT(out_flags) = win_protect_table[mask];
+    return OS_OK;
+}
+
+os_result os_mem_allocate(void** out_pointer, void* start_address, os_size size, os_mem_alloc_intents alloc_intents, os_mem_protect_intents protect_intents) {
+    DWORD alloc = 0;
+    DWORD prot = 0;
+
+    os_result res = translate_protect_intents(&prot, alloc_intents);
+    if (res != OS_OK) return res;
+
+    if (alloc_intents & OS_MEM_LARGE_PAGES) {
+        if (!((alloc_intents & OS_MEM_RESERVE) && (alloc_intents & OS_MEM_COMMIT))) {
+            return OS_ERROR_INVALID_ARGUMENT;
+        }
+        alloc = MEM_RESERVE | MEM_COMMIT | MEM_LARGE_PAGES;
+    } else {
+        if (alloc_intents & OS_MEM_RESERVE) alloc |= MEM_RESERVE;
+        if (alloc_intents & OS_MEM_COMMIT) alloc |= MEM_COMMIT;
+    }
+
+    if (alloc == 0) return OS_ERROR_INVALID_ARGUMENT;
+
+    void* pointer = VirtualAlloc(start_address, size, alloc, prot);
+    if (pointer == NULL) {
+        DWORD error = GetLastError();
+        switch (error) {
+            case ERROR_NOT_ENOUGH_MEMORY: return OS_ERROR_NO_MEMORY;
+
+            case ERROR_BAD_ARGUMENTS:
+            case ERROR_INVALID_PARAMETER:
+            case ERROR_INVALID_ADDRESS:
+                return OS_ERROR_INVALID_ARGUMENT;
+
+            default: return OS_UNKNOWN_ERROR;
+        }
+    }
+
+    *out_pointer = pointer;
+
+    return OS_OK;
+}
+
+os_result os_mem_free(void* address, os_size size, os_mem_free_intents free_intents) {
+    WINBOOL res;
+    if (free_intents == OS_MEM_DECOMMIT) {
+        res = VirtualFree(address, size, MEM_DECOMMIT);
+    } else if (free_intents == OS_MEM_RELEASE) {
+        res = VirtualFree(address, size, MEM_RELEASE);
+    } else {
+        return OS_ERROR_INVALID_ARGUMENT;
+    }
+
+    if (res == 0) { // nonzero = error for VirtualFree
+        DWORD error = GetLastError();
+        switch (error) {
+            case ERROR_INVALID_PARAMETER:
+            case ERROR_BAD_ARGUMENTS:
+            case ERROR_INVALID_ADDRESS:
+                return OS_ERROR_INVALID_ARGUMENT;
+
+            default: return OS_UNKNOWN_ERROR;
+        }
+    }
+
+    return OS_OK;
 }
