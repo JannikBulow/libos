e7e86e3253f68d8098103c01bc1b626704942123
JesusTouchMe <dodoakmishot@gmail.com>
2026-03-24 09:25:41 +0100

    fix(memory): add _enum suffix to intent enums to fix C++ type redefine errors

--- include/libos/memory.h
+++ include/libos/memory.h
@@ -12,7 +12,7 @@
 extern "C" {
 #endif
 
-enum os_mem_alloc_intents {
+enum os_mem_alloc_intents_enum {
     OS_MEM_RESERVE      = 1 << 0,
     OS_MEM_COMMIT       = 1 << 1,
     OS_MEM_LARGE_PAGES  = 1 << 2, // If you specify this intent, you must also specify RESERVE and COMMIT
@@ -20,12 +20,12 @@ enum os_mem_alloc_intents {
     OS_MEM_ZERO         = 1 << 3,
 };
 
-enum os_mem_free_intents {
+enum os_mem_free_intents_enum {
     OS_MEM_DECOMMIT     = 1 << 0,
     OS_MEM_RELEASE      = 1 << 1,
 };
 
-enum os_mem_protect_intents {
+enum os_mem_protect_intents_enum {
     OS_MEM_PROTECT_NOACCESS             = 1 << 0,
     OS_MEM_PROTECT_READ                 = 1 << 1,
     OS_MEM_PROTECT_READWRITE            = 1 << 2, //NOTE: I am a fan of having the control to make write-only memory, but please name one situation where that is needed.
